594f8125d41d45b2773f3939f0b50a1bec0cec3a
Mohit Reddy <mohitreddy1996@gmail.com>
2017-02-26 18:57:04 +0530

    Server Implementation started

--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -2,10 +2,34 @@
 
 package lsp
 
-import "errors"
+import (
+	"errors"
+	"../lspnet"
+	"time"
+	"strconv"
+	"fmt"
+	"encoding/json"
+)
 
 type server struct {
-	// TODO: implement this!
+	winSize int
+	connectionId int
+	serverConnection *lspnet.UDPConn
+	ticker *time.Ticker
+	epochLimit int
+	quitAllChannel chan bool
+	AddrChannel chan *lspnet.UDPAddr
+	dataChannel chan *Message
+	ackChannel chan *Message
+	resendChannel chan bool
+	closeChannel chan int
+	writeChannel chan WriteData
+}
+
+type WriteData struct {
+	connId int
+	seqNum int
+	payload []byte
 }
 
 // NewServer creates, initiates, and returns a new server. This function should
@@ -15,7 +39,24 @@ type server struct {
 // project 0, etc.) and immediately return. It should return a non-nil error if
 // there was an error resolving or listening on the specified port number.
 func NewServer(port int, params *Params) (Server, error) {
-	return nil, errors.New("not yet implemented")
+	myServer := server{
+		winSize: params.WindowSize,
+		connectionId: 0,
+		ticker: time.NewTicker(time.Millisecond*time.Duration(params.EpochMillis)),
+		epochLimit: params.EpochLimit,
+		quitAllChannel: make(chan bool),
+		AddrChannel: make(chan *lspnet.UDPAddr),
+		dataChannel: make(chan *Message),
+		ackChannel: make(chan *Message),
+		resendChannel: make(chan bool),
+		closeChannel: make(chan int),
+		writeChannel: make(chan WriteData),
+	}
+	var err error
+	go func() {
+		err = myServer.StartListen(strconv.Itoa(port))
+	}()
+	return &myServer, err
 }
 
 func (s *server) Read() (int, []byte, error) {
@@ -25,13 +66,81 @@ func (s *server) Read() (int, []byte, error) {
 }
 
 func (s *server) Write(connID int, payload []byte) error {
-	return errors.New("not yet implemented")
+	s.writeChannel <- WriteData{connID, 0, payload}
+	return nil
 }
 
 func (s *server) CloseConn(connID int) error {
-	return errors.New("not yet implemented")
+	go func() {
+		s.closeChannel <- connID
+	}()
+	return nil
 }
 
 func (s *server) Close() error {
 	return errors.New("not yet implemented")
+}
+
+func (s *server) StartListen(port string) error {
+	UDPAddr, err := lspnet.ResolveUDPAddr("udp", ":"+port)
+	if err!=nil{
+		fmt.Println("Unable to resolve UDP Address ", port)
+		return err
+	}
+	s.serverConnection, err = lspnet.ListenUDP("udp", UDPAddr)
+	if err!=nil{
+		fmt.Println("Unable to listen on Port ", port)
+		return err
+	}
+
+	go s.ticking()
+	go s.handleMessages()
+
+	for{
+		select {
+		case <-s.quitAllChannel:
+			fmt.Println("Server Quit Listener")
+			return nil
+		default:
+			// check and read message from UDP connection and check its types.4
+			// write handle messages to operate on respective channels.
+			buf := make([]byte, 2000)
+			n, addr, err:= s.serverConnection.ReadFromUDP(buf)
+			if err!=nil{
+				return err
+			}
+			msg := Message{}
+			json.Unmarshal(buf[0:n], &msg)
+			switch msg.Type {
+			case MsgConnect:
+				s.AddrChannel <- addr
+			case MsgData:
+				s.dataChannel <- &msg
+			case MsgAck:
+				s.ackChannel <- &msg
+			}
+		}
+	}
+	return err
+}
+
+func (s *server) ticking() {
+	for{
+		select {
+		case <-s.quitAllChannel:
+			fmt.Println("Server Quit Ticket")
+			s.ticker.Stop()
+			return
+		case <-s.ticker.C:
+			s.resendChannel <- true
+		}
+	}
+}
+
+func (s *server) handleMessages(){
+	for{
+		select {
+		// handle all channel cases. All resend, receive message etc.
+		}
+	}
 }
